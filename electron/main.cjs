5b77afba348e603e4b427e791e7e66e019baa805
Mark de Valk <mark.de.valk@gmail.com>
2026-07-16 16:28:44 +0200

    fix: remove automatic DevTools on startup

--- electron/main.cjs
+++ electron/main.cjs
@@ -20,7 +20,6 @@ function createWindow() {
 
   if (isDev) {
     win.loadURL('http://localhost:5174')
-    win.webContents.openDevTools()
   } else {
     win.loadFile(path.join(__dirname, '../dist/index.html'))
   }
